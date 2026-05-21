cca3e4319300335b68f8c8dc5c31a01a62d64643
Filippo Dodi <fillo.dodi@gmail.com>
2026-05-21 15:38:02 +0200

    configurable gemini timeout

--- src/services/gemini.service.ts
+++ src/services/gemini.service.ts
@@ -135,4 +135,4 @@ class GeminiService {
     }
 }
 
-export const geminiService = new GeminiService({ apiKey: ENV.GEMINI_API_KEY })
+export const geminiService = new GeminiService({ apiKey: ENV.GEMINI_API_KEY, defaultTimeoutMs: ENV.GEMINI_TIMEOUT_MS })
